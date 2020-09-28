2ce657d54e8523a99b7d7226adb5cf079aad0030
KPKanimator <kirill.korobchinskiy@gmail.com>
2020-09-28 15:47:42 +0300

    Lesson 1 25/09/2020

---
+++ Lesson01/Demo001/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 10;
+	cout << "x = ";
+	cin >> x;
+	cout << "x = " << x << endl;
+
+	cout << "Hello peoples!!!" << endl;
+
+	system("pause");
+	return 0;
+}
