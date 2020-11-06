371e1f73b95991b05dc219216ec4a6112d89b732
KPKanimator <kirill.korobchinskiy@gmail.com>
2020-11-06 09:41:56 +0200

    Lecture 12

---
+++ Lesson12/Demo001/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double y;
+  const double TOPX = 2.;
+  const double DOWNX = -2.;
+  const double STEP = 0.5;
+  int counter = 0;
+
+  cout << "-----------------\n";
+  cout << "n\tx\ty\n";
+  cout << "-----------------\n";
+  //for (double x = DOWNX, int count = 0; x < TOPX; x += STEP, count++)
+  for (double x = DOWNX; x <= TOPX; x += STEP, ++counter)
+  { 
+    // вывести чётные итерации
+    if(counter % 6 == 0 and counter!= 0)
+      break;
+      //continue;
+    y = -2.4 * x * x + 5 * x - 3;
+    cout << counter + 1 << "\t" << x << "\t" << y << endl;
+  }
+  cout << "-----------------\n";
+
+  system("pause");
+  return EXIT_SUCCESS;
+}
