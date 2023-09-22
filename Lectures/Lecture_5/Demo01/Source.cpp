be495ba677d189786499314eaa8b24a97b2c001f
Kirill Korobchinskiy <kirill.korobchinskiy@gmail.com>
2023-09-22 09:36:56 +0300

    First program

---
+++ Lectures/Lecture_5/Demo01/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void print_name()
+{
+	cout << "My name Kir" << endl;
+}
+
+int main()
+{
+	print_name();
+	int a = 0;
+	a = 10 + a;
+	cout <<" a = " << a << endl << endl;
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
+
+/*
+My name Kir
+ a = 10
+
+*/
+
