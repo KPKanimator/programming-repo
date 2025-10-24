c22c155ad06052c1322b4c8a144651d6e3924e2f
Kirill Korocbhinskiy <k.korobchinskiy@khai.edu>
2025-10-24 08:39:16 +0300

    Add preset.cpp

---
+++ Lectures/preset.cpp
@@ -0,0 +1,36 @@
+/*
+ *
+ * Date: //2025
+ * Author: Kirill Korobchinsky
+ */
+#define _USE_MATH_DEFINES
+
+#pragma region Includes
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <stdlib.h>
+#include <locale>
+#include "windows.h"
+using namespace std;
+#pragma endregion
+
+int main()
+{
+#pragma region Ukranian
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Ukrainian");
+#pragma endregion
+
+
+
+	system("pause>nul");
+	return EXIT_SUCCESS;
+}
+
+/* ------  RESULT  -------
+
+
+*/
+
